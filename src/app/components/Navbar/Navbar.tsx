a334cf32f077d96d2677a0d593bb819c42a91ff6
Spencer Haka <spencer.haka@gmail.com>
2024-07-01 12:37:03 -0600

    Update FAQ page label, and navbar font weight and size

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -15,14 +15,14 @@ function Navbar() {
   return (
     <div id="navbar" className='flex position: sticky top-0 justify-between w-[95%] mt-4 p-3 bg-lightBlue rounded-md z-10'>
       <ul className='flex justify-evenly w-1/2'>
-        <li className='navlink'><a href='#About'>About</a></li>
-        <li className='navlink'><a href='#Services'>Services</a></li>
-        <li className='navlink'><a href='#FAQ'>FAQ</a></li>
+        <li className='navlink font-sans font-extralight text-2xl'><a href='#About'>About</a></li>
+        <li className='navlink font-sans font-extralight text-2xl'><a href='#Services'>Services</a></li>
+        <li className='navlink font-sans font-extralight text-2xl'><a href='#FAQ'>FAQ</a></li>
       </ul>
       <ul className='flex justify-evenly w-1/2'>
-        <li className='navlink'><a href='#Contact'>Contact</a></li>
-        <li className='navlink'><a href='#Resources'>Resources</a></li>
-        <li className='navlink'><a href='#Location'>Location</a></li>
+        <li className='navlink font-sans font-extralight text-2xl'><a href='#Contact'>Contact</a></li>
+        <li className='navlink font-sans font-extralight text-2xl'><a href='#Resources'>Resources</a></li>
+        <li className='navlink font-sans font-extralight text-2xl'><a href='#Location'>Location</a></li>
       </ul>
     </div>
   )
