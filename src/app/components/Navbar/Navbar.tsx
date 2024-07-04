bfa5a400da158f5a1e6ffaeac843a5d81b8c6724
Spencer Haka <spencer.haka@gmail.com>
2024-07-04 08:56:01 -0600

    About section responsiveness

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -12,16 +12,16 @@ function Navbar() {
   // }
 
   return (
-    <div id="navbar" className='flex position: sticky top-0 justify-between w-[95%] mt-4 p-3 bg-lightBlue rounded-md z-10'>
+    <div id="navbar" className='flex position: sticky top-0 justify-between w-[95%] mt-4 p-3 bg-lightBlue rounded-md z-10 text-2xl max-xl:text-xl max-lg:text-lg max-md:text-sm max-sm:text-xs'>
       <ul className='flex justify-evenly w-1/2'>
-        <li className='navlink font-sans font-extralight text-2xl'><a href='#About'>About</a></li>
-        <li className='navlink font-sans font-extralight text-2xl'><a href='#Services'>Services</a></li>
-        <li className='navlink font-sans font-extralight text-2xl'><a href='#FAQ'>FAQ</a></li>
+        <li className='navlink font-sans font-extralight'><a href='#About'>About</a></li>
+        <li className='navlink font-sans font-extralight'><a href='#Services'>Services</a></li>
+        <li className='navlink font-sans font-extralight'><a href='#FAQ'>FAQ</a></li>
       </ul>
       <ul className='flex justify-evenly w-1/2'>
-        <li className='navlink font-sans font-extralight text-2xl'><a href='#Contact'>Contact</a></li>
-        <li className='navlink font-sans font-extralight text-2xl'><a href='#Resources'>Resources</a></li>
-        <li className='navlink font-sans font-extralight text-2xl'><a href='#Location'>Location</a></li>
+        <li className='navlink font-sans font-extralight'><a href='#Contact'>Contact</a></li>
+        <li className='navlink font-sans font-extralight'><a href='#Resources'>Resources</a></li>
+        <li className='navlink font-sans font-extralight'><a href='#Location'>Location</a></li>
       </ul>
     </div>
   )
