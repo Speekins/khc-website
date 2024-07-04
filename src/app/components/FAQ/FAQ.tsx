d4faae6c3adb60d2b0894812d7ce96d424ad0401
Spencer Haka <spencer.haka@gmail.com>
2024-07-04 08:21:12 -0600

    Fix FAQ responsiveness

--- src/app/components/FAQ/FAQ.tsx
+++ src/app/components/FAQ/FAQ.tsx
@@ -9,7 +9,7 @@ function FAQ() {
       <p className='w-1/2 p-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <div className='relative w-1/2'>
         <Image alt='headshot' src={bricks} />
-        <h1 className='absolute top-1/2 left-10 text-slate text-9xl font-thin'>FAQ</h1>
+        <h1 className='absolute top-1/2 left-10 text-slate text-9xl font-thin max-xl:text-8xl max-lg:text-7xl max-md:text-6xl'>FAQ</h1>
       </div>
     </div>
   )
