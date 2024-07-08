8f326e926725ab40064e50700c92484c73d7f6b9
Spencer Haka <spencer.haka@gmail.com>
2024-07-07 23:13:11 -0600

    Location had a fixed with on iFrame. Changed to responsive in order to fix overflow issue

--- src/app/components/Location/Location.tsx
+++ src/app/components/Location/Location.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Location() {
   return (
-    <div id='Location' className='flex flex-col items-center justify-center mt-10'>
+    <div id='Location' className='flex flex-col items-center justify-center mt-10 w-screen'>
       <h1 className='text-8xl font-thin mb-6 max-xl:text-8xl max-lg:text-7xl max-md:text-6xl'>Location</h1>
-      <iframe className="m-8 w-[800px]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d757.8232221168776!2d-105.01233633700681!3d39.6135079921206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c81cad6a2824d%3A0x40aee1320dd2aef3!2s2009%20W%20Littleton%20Blvd%2C%20Littleton%2C%20CO%2080120!5e0!3m2!1sen!2sus!4v1697595046681!5m2!1sen!2sus" width="600" height="450" loading="lazy"></iframe>
+      <iframe className="w-5/6" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d757.8232221168776!2d-105.01233633700681!3d39.6135079921206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c81cad6a2824d%3A0x40aee1320dd2aef3!2s2009%20W%20Littleton%20Blvd%2C%20Littleton%2C%20CO%2080120!5e0!3m2!1sen!2sus!4v1697595046681!5m2!1sen!2sus" width="600" height="450" loading="lazy"></iframe>
     </div>
   )
 }
