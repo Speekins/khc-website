16cca2365cd986b6a6245c57de79929a82b01f0a
Spencer Haka <spencer.haka@gmail.com>
2024-07-09 09:05:20 -0600

    Fix responsiveness of Services h1

--- src/app/components/Services/Services.tsx
+++ src/app/components/Services/Services.tsx
@@ -4,7 +4,7 @@ function Services() {
   return (
     <div id='Services' className='flex items-center justify-between w-screen mt-12 h-screen'>
       <div className='flex bg-services bg-cover w-1/2 h-full max-sm:w-1/4 max-sm:justify-end'>
-        <h1 className='p-10 text-white text-9xl font-thin max-xl:text-8xl max-lg:text-7xl max-md:text-6xl max-sm:rotate-90 max-sm:mr-20'>Services</h1>
+        <h1 className='p-10 text-white text-9xl font-thin max-xl:text-8xl max-lg:text-7xl max-md:text-6xl max-sm:rotate-90 max-sm:m-24 max-[460px]:m-20 max-[380px]:m-14'>Services</h1>
       </div>
       <div className='flex items-center justify-evenly w-1/2 h-3/4 max-sm:w-3/4 max-[500px]:w-2/3'>
         <ul className='flex flex-col justify-evenly text-3xl font-extralight list-disc h-full max-xl:text-2xl max-md:text-xl max-sm:text-lg max-[500px]:text-md'>
