2174baad5b0e74dbd07bfee2f5e129fc530dd4ab
Spencer Haka <spencer.haka@gmail.com>
2024-07-09 19:40:32 -0600

    Update Services lists

--- src/app/components/Services/Services.tsx
+++ src/app/components/Services/Services.tsx
@@ -7,16 +7,25 @@ function Services() {
         <h1 className='h-4 mt-10 text-white text-9xl font-thin max-xl:text-8xl max-lg:text-7xl max-md:text-6xl max-sm:text-5xl max-sm:rotate-90 max-sm:translate-y-40'>Services</h1>
       </div>
       <div className='flex items-center justify-evenly w-1/2 h-3/4 max-sm:w-3/4 max-[500px]:w-2/3'>
-        <ul className='flex flex-col justify-evenly text-3xl font-extralight list-disc h-full max-xl:text-2xl max-md:text-xl max-sm:text-lg max-[500px]:text-md'>
-          <li className=''>Post Partum Mothers</li>
-          <li className=''>Anxiety</li>
-          <li className=''>Depression</li>
-          <li className=''>Grief & Loss</li>
-          <li className=''>Life Transitions</li>
-          <li className=''>EMDR</li>
-          <li className=''>Motivational Inteviewing</li>
-          <li className=''>Cognitive Behavioral Therapy</li>
-        </ul>
+        <div className='flex flex-col h-full w-1/3'>
+          <label htmlFor='treated-diagnoses' className='text-2xl'>Treated Diagnoses</label>
+          <ul id='treated-diagnoses' className='flex flex-col justify-evenly text-xl font-extralight list-disc h-full max-xl:text-lg max-md:text-md'>
+            <li className=''>Post Partum Mothers</li>
+            <li className=''>Anxiety</li>
+            <li className=''>Depression</li>
+            <li className=''>Grief & Loss</li>
+            <li className=''>Life Transitions</li>
+          </ul>
+        </div>
+        <div className='flex flex-col h-full w-1/3'>
+        <label htmlFor='modalities' className='text-2xl'>Modalities</label>
+          <ul id='modalities'className='flex flex-col justify-evenly text-xl font-extralight list-disc h-full max-xl:text-lg max-md:text-md'>
+            <li className=''>EMDR</li>
+            <li className=''>Cognitive Behavioral Therapy</li>
+            <li className=''>Internal Family Systems</li>
+            <li className=''>Motivational Inteviewing</li>
+          </ul>
+        </div>
       </div>
     </div>
   )
